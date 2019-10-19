e3ecf3cd725b2ea66dace24ad6e79c37aa7535f7
Anna <annashkt@gmail.com>
2019-10-19 03:53:25 +0300

    feat: guessing-game solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.lastGuess = Math.round((this.max - this.min) / 2) + this.min;
+        return this.lastGuess;
     }
 
     lower() {
-
+        this.max = this.lastGuess;
     }
 
     greater() {
-
+        this.min = this.lastGuess;
     }
 }
 
